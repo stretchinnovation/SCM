347057bbecb0ccd2cefd605c5e0f93fcae042276
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-11 20:32:58 +0000

    Add csv uploader

--- SCM.py
+++ SCM.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import streamlit as st
 
-print ("Hello world!")
+#print ("Hello world!")
+
+# File uploader
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    #st.subheader("Preview of Data")
+    st.dataframe(df.head())
