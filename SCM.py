fb975a829ed4edb033ee7b6d2b09da40efde6f09
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-11 22:30:46 +0000

    try new upload

--- SCM.py
+++ SCM.py
@@ -4,7 +4,8 @@ import pandas as pd
 #st.title("CSV Splitter")
 
 # Upload CSV file
-uploaded_file = st.file_uploader("", type="csv")
+#uploaded_file = st.file_uploader("", type="csv")
+uploaded_file = pd.read_csv(r"C:\Users\rowan\Desktop\CSM Streamlit\*.csv")
 
 if uploaded_file is not None:
     # Read raw lines from the uploaded file
