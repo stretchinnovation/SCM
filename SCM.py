aab02bd529c4cf389b38b574f3985ebc7aa25131
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-11 22:42:09 +0000

    try new path

--- SCM.py
+++ SCM.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 import glob
-files = glob.glob("PICKUP/*.csv")
+files = glob.glob("SCM/PICKUP/*.csv")
 df = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
 
 # Upload CSV file
