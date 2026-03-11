39f7d9bbdaf8af8595fdd57e97682cfc8e3963ea
stretchinnovation <it.stretchinnovation@gmail.com>
2026-03-11 22:39:19 +0000

    try new repo path

--- SCM.py
+++ SCM.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 import glob
-files = glob.glob(r"C:\Users\rowan\Desktop\CSM Streamlit\pickup\*.csv")
+files = glob.glob("PICKUP/*.csv")
 df = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
 
 # Upload CSV file
